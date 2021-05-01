75c4067c105597d1d3a2242a2206c689f4e4543a
Abdulwasiu Abdulmuize <AOMuiz@users.noreply.github.com>
2021-05-01 20:56:35 +0100

    edit config js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `AOMUIZ Portfolio`,
+        short_name: `AOMUIZ`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
